548c0a5a269bba48afae033321d6d9e43a544205
CH34LL3 <obiekechealle@gmail.com>
2026-02-27 17:39:34 +0100

    Babe is now Brat

--- index.js
+++ index.js
@@ -2,97 +2,107 @@
 const arg = require('arg');
 const fs = require('node:fs')
 const path = require('node:path')
-const readline= require('node:readline')
+const readline = require('node:readline')
 
 
 
 
 const args = arg({
-	
-	'--start': Boolean,
+
+	'--sup': Boolean,
 	'--build': Boolean,
+	'--huh': Boolean,
 	'--patt': String,
 	'--remb': String,
 	'--hate': String,
 	'--GMYP': Boolean,
+	'--h': '--huh'
 });
 
 
-if (args['--start']){
-	console.log("Heyyyy~")
-	
+if (args['--sup']) {
+	console.log("get out of my folder")
+
+
 }
- if (args['--patt']){
- 
-	if (args['--patt'].includes('\\')){
-		console.log("ugh, this is boring")
-		const storage = path.join(__dirname, 'storage.json')
-		const d =  JSON.parse(fs.readFileSync(storage,'utf8'))
-		d.cwd = args['--patt']
-		fs.writeFileSync(storage,JSON.stringify(d))
-	}
-	else{
-		const storage = path.join(__dirname, 'storage.json')
-		const d =  JSON.parse(fs.readFileSync(storage,'utf8'))
-		
-		if (d.projects[args['--patt']]){
-			d.cwd = d.projects[args['--patt']]
-			console.log(`See, I told you I was listening! \n You're now in Project:${d.cwd} | >:3`)
-			fs.writeFileSync(storage,JSON.stringify(d))
-			
+
+if (args['--huh']) {
+	console.log(`Commands: \n --huh : Show all commands \n --sup : Kinda does nothing lowk \n --patt : changes the CWF \n --remb : Add project location to memory \n --hate : remove a project from memory \n --GMYP : Makes her spill her guts`)
+}
+if (args['--patt']) {
+	try {
+		if (args['--patt'].includes('\\')) {
+			console.log("ugh, this is boring")
+			const storage = path.join(__dirname, 'storage.json')
+			const d = JSON.parse(fs.readFileSync(storage, 'utf8'))
+			d.cwf = args['--patt']
+			fs.writeFileSync(storage, JSON.stringify(d))
 		}
-		else {console.log("Someone has dementia already. . .")}
-		
+		else {
+			const storage = path.join(__dirname, 'storage.json')
+			const d = JSON.parse(fs.readFileSync(storage, 'utf8'))
+
+			if (d.projects[args['--patt']]) {
+				d.cwf = d.projects[args['--patt']]
+				console.log(`Yes yes, \n You're now in Project: ${d.cwf} \n Leave me alone`)
+				fs.writeFileSync(storage, JSON.stringify(d))
+
+			}
+			else { console.log("Someone has dementia already. . .") }
+
+		}
+	}
+	catch (error) {
+		console.log("You are really wasting my time here.", error)
 	}
 }
 
-if (args['--hate']){
+if (args['--hate']) {
 	const storage = path.join(__dirname, 'storage.json')
-	const d =  JSON.parse(fs.readFileSync(storage,'utf8'))
-	
-	if(d.projects[args['--hate']]){
+	const d = JSON.parse(fs.readFileSync(storage, 'utf8'))
+
+	if (d.projects[args['--hate']]) {
 		delete d.projects[args['--hate']]
-		console.log("I kinda liked that one...")
-		fs.writeFileSync(storage,JSON.stringify(d))
+		console.log("Finally! Sorry, I didn't want to tell you how ugly that thing was...")
+		fs.writeFileSync(storage, JSON.stringify(d))
 	}
-	else{console.log("You're fighting ghost darling")}
+	else { console.log("You're fighting ghost lil bro") }
 }
- if (args['--remb']){
+if (args['--remb']) {
 	const storage = path.join(__dirname, 'storage.json')
-	const d =  JSON.parse(fs.readFileSync(storage,'utf8'))
-	
+	const d = JSON.parse(fs.readFileSync(storage, 'utf8'))
+
 	const rl = readline.createInterface({
-	  input: process.stdin,
-	  output: process.stdout,
+		input: process.stdin,
+		output: process.stdout,
 	});
 
-	
-	rl.question(`HUH? oh yeah, I was TOTALLY listening. . . what'd you call it again? haha: `, name => {
-	d.projects[name] = args['--remb']; 
-	
-	console.log(d.projects)
-	fs.writeFileSync(storage, JSON.stringify(d))
-	rl.close()
+
+	rl.question(`I should charge a fee for this. Whatever, what do I call it?: `, name => {
+		d.projects[name] = args['--remb'];
+
+		console.log(d.projects)
+		fs.writeFileSync(storage, JSON.stringify(d))
+		rl.close()
 	})
 
 }
 if (args['--build']) {
-	try{
-	console.log("wait it's giving! This my fav part")
-	const storage = path.join(__dirname, 'storage.json')
-	const d =  JSON.parse(fs.readFileSync(storage,'utf8'))
-	require(path.resolve(d.cwd))
+	try {
+		console.log("ughhhhhhh")
+		const storage = path.join(__dirname, 'storage.json')
+		const d = JSON.parse(fs.readFileSync(storage, 'utf8'))
+		require(path.resolve(d.cwf))
+		console.log("Wait, maybe you're not that much of a loser. . .")
+
 	}
-	catch (error){console.log("HAHA! That's not a not a JS file stupid!")}
+	catch (error) { console.log(`You're so fucking stupid lmfao \n ${error}`) }
 }
 
-
-
-if (args['--GMYP']){
+if (args['--GMYP']) {
 	const storage = path.join(__dirname, 'storage.json')
-	const d = JSON.parse(fs.readFileSync(storage,'utf8'))
-	console.log(`Gosh, you're so controlling! Here you go dickwad`)
+	const d = JSON.parse(fs.readFileSync(storage, 'utf8'))
+	console.log(`GEE! Ever heard of , i dunno, PRIVACY??`)
 	console.dir(d)
-	
+
 }
-// D:\Desktop\Science-y shit\NL Coding\Matrix Mulitplier\Square Matrix Multiplier\Matrix Multiplier.js
